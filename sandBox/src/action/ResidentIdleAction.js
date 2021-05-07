8bd97026b4ac577191c5782209a9f159ed434d81
sw28363807 <sw849557369@gmail.com>
2021-05-07 21:48:37 +0800

    no message

--- sandBox/src/action/ResidentIdleAction.js
+++ sandBox/src/action/ResidentIdleAction.js
@@ -1,12 +1,13 @@
 export default class ResidentIdleAction {
-
+    // 创建待机动画
     static createAction(owner) {
+        Laya.Tween.clearAll(owner);
         let time = 1000;
         let scaleBig = 1;
         let scaleSmall = 0.8;
         let func = function() {
-            Laya.Tween.to(owner, {scaleY:scaleSmall}, time, null, Laya.Handler.create(this, function() {
-                Laya.Tween.to(owner, {scaleY:scaleBig}, time, null, Laya.Handler.create(this, function() {
+            Laya.Tween.to(owner, {scaleY:scaleSmall}, time, null, Laya.Handler.create(owner, function() {
+                Laya.Tween.to(owner, {scaleY:scaleBig}, time, null, Laya.Handler.create(owner, function() {
                 
                 }), 0, true, true);
             }), 0, true, true);
