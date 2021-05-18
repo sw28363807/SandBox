21a1102ea43bf17132f89c1f69b77ed8f1f29aa8
sw28363807 <sw849557369@gmail.com>
2021-05-18 12:46:46 +0800

    no message

--- sandBox/src/panel/CommandPanel.js
+++ sandBox/src/panel/CommandPanel.js
@@ -6,11 +6,28 @@ export default class CommandPanel extends Laya.Script {
     
     onEnable() {
         this.listView = this.owner.getChildByName("_list");
-        // new Laya.List().vs
         this.listView.array = [1,2,3,1,2,3,1,2,3,1,2,3,1,2,3];
         this.listView.refresh();
+
+        this.upBtn = this.owner.getChildByName("upBtn");
+        this.downBtn = this.owner.getChildByName("downBtn");
+        this.downBtn.visible = false;
+        this.listView.scaleY = 0;
+        this.downBtn.on(Laya.Event.CLICK, this, function () {
+            this.downBtn.visible = false;
+            this.upBtn.visible = true;
+            Laya.Tween.to(this.listView, {scaleY: 0}, 200, Laya.Ease.backIn);
+            
+        });
+
+        this.upBtn.on(Laya.Event.CLICK, this, function () {
+            this.downBtn.visible = true;
+            this.upBtn.visible = false;
+            Laya.Tween.to(this.listView, {scaleY: 1}, 200, Laya.Ease.backOut);
+        });
     }
 
     onDisable() {
+
     }
 }
