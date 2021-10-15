1482ead9c51070071b432a683842961182f8da45
sw28363807 <sw849557369@gmail.com>
2021-10-15 18:14:25 +0800

    no message

--- sandBox/src/building/FactoryLogic.js
+++ sandBox/src/building/FactoryLogic.js
@@ -20,36 +20,4 @@ export default class FactoryLogic extends BuildingBaseLogic {
             scene.selectedBuilding = this.owner;
         }));
     }
-
-    getCurWorkersIdArray() {
-        let workersArray = this.getModel().getExteraData("workersArray");
-        if (workersArray == undefined || workersArray == null) {
-            workersArray = [];
-        }
-        return workersArray;
-    }
-
-    addWorkerId(workerId) {
-        let model = this.getModel();
-        let workersArray = this.getCurWorkersIdArray();
-        for (let index = 0; index < workersArray.length; index++) {
-            const item = workersArray[index];
-            if (String(item) == String(workerId)) {
-                return
-            }
-        }
-        workersArray.push(workerId);
-        model.setExteraData("workersArray", workersArray);
-    }
-
-    // // 设置当前装饰物
-    // setCurDecoNum(itemId, num) {
-    //     let model = this.getModel();
-    //     let curDecos = model.getExteraData("curDecos");
-    //     if (curDecos == undefined || curDecos == null) {
-    //         curDecos = {};
-    //     }
-    //     curDecos[String(itemId)] = num;
-    //     model.setExteraData("curDecos", curDecos);
-    // }
 }
--- sandBox/src/scene/FactoryDialogLogic.js
+++ sandBox/src/scene/FactoryDialogLogic.js
@@ -20,56 +20,5 @@ export default class FactoryDialogLogic extends Laya.Script {
         this.closeBtn.on(Laya.Event.CLICK, this, function () {
             Laya.Dialog.close(ResourceMeta.FactoryDialogScenePath);
         });
-        this.chilun1 = this.owner.getChildByName("chilun1");
-        this.chilun2 = this.owner.getChildByName("chilun2");
-        this.manAni = this.owner.getChildByName("man");
-        this.womanAni = this.owner.getChildByName("woman");
-        this.rotation1 = 0;
-        this.rotation2 = 0;
-        this.refreshFactor();
     }
-
-    refreshFactor() {
-        let workersArray = this.buildingScript.getCurWorkersIdArray();
-        let workersArrayLength = workersArray.length;
-        let meta = BuildingMeta.BuildingDatas[BuildingMeta.BuildingType.FactoryType];
-        let numText = this.owner.getChildByName("numText");
-        numText.text = String(workersArrayLength) + "/" + String(meta.maxResident);
-        let descText = this.owner.getChildByName("descText");
-        if (workersArrayLength == 0) {
-            descText.text = "当前没有工人在工作";
-        } else {
-            descText.text = "工人们正在努力工作";
-        }
-        let lastWorking = this.isWorking;
-        this.isWorking = (workersArrayLength != 0);
-        if (lastWorking != this.isWorking) {
-            if (this.isWorking) {
-                this.manAni.play(0, true, "enjoy");
-                this.womanAni.play(0, true, "enjoy");
-            } else {
-                this.manAni.play(0, true, "idle");
-                this.womanAni.play(0, true, "idle");
-            }
-        }
-    }
-
-    onUpdate() {
-        if (this.isWorking == true) {
-            if (this.rotation1 >= 360) {
-                this.rotation1 = 0;
-            }
-            if (this.rotation2 < -360) {
-                this.rotation2 = 0;
-            }
-            this.chilun1.rotation = this.rotation1;
-            this.chilun2.rotation = this.rotation2;
-            this.rotation1 += 2;
-            this.rotation2 -= 4;
-        } else {
-            this.rotation1 = 0;
-            this.rotation2 = 0;
-        }
-    }
-
 }
