cac0c3ffac2600fe0e6fac226fb6e0f12d206756
sw28363807 <sw849557369@gmail.com>
2021-10-20 16:49:20 +0800

    no message

--- sandBox/src/meta/QuestionMeta.js
+++ sandBox/src/meta/QuestionMeta.js
@@ -20,4 +20,40 @@ QuestionMeta.question = [
         ],
         answer: 2,
     },
+    {
+        question: "下面动物中不会流汗的是",
+        option: [
+            "马马",
+            "猫咪",
+            "狗狗",
+        ],
+        answer: 2,
+    },
+    {
+        question: "被誉为“世界油王”的是",
+        option: [
+            "油棕~",
+            "大豆~",
+            "花生~",
+        ],
+        answer: 0,
+    },
+    {
+        question: "沙生植物的根很长，是因为",
+        option: [
+            "抗风刮~",
+            "抗干旱~",
+            "以上二者皆是~",
+        ],
+        answer: 2,
+    },
+    {
+        question: "沙生植物的根很长，是因为",
+        option: [
+            "抗风刮~",
+            "抗干旱~",
+            "以上二者皆是~",
+        ],
+        answer: 2,
+    },
 ];
